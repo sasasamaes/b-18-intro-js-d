3a5ce1c4d07a26162c1794e406ad5dc2394bbe93
Francisco Campos <hey@francampos.me>
2022-07-05 17:11:41 -0600

    init

---
+++ intro-js/js/main.js
@@ -0,0 +1,40 @@
+//Salidas
+console.log("Hola grupo b18")
+console.log(4+5)
+console.log(4>3)
+
+document.write("Hola grupo b18")
+
+// alert("Hola grupo b18")
+// Entradas
+// alert("Hola " +prompt("Cual es tu nombre?"))
+
+// var infoUsuario = prompt("Cual es tu nombre?")
+
+
+// console.log("Hola, " + infoUsuario + " Estamos en clase del grupo b18")
+
+// var num1 = prompt("Cual es el numero 1")
+// var num2 = prompt("Cual es el numero 2")
+
+// alert(parseInt(num1) + parseInt(num2))
+
+
+
+
+// var nombre = prompt("Cual es tu nombre?")
+
+// var apellido = prompt("Cual es tu apellido?")
+
+// console.log()
+
+
+
+
+
+
+
+
+
+
+
